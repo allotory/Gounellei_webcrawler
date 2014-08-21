450f5685e71e41661405b0f5ac3594a27e591c2a
Ellery <allotory@msn.com>
2014-08-21 16:30:00 +0800

    finish basic function

--- qiubai/qiubai_webcrawler.py
+++ qiubai/qiubai_webcrawler.py
@@ -8,34 +8,71 @@ QiuShiBaiKe web crawler.
 
 import urllib2
 import re
+import time
+import thread
 
 
 class QiubaiCrawler(object):
 	def __init__(self):
 		self.page = 1
+		self.pages = []
+		self.enable = False
 
 	def get_page_content(self, page):
-		qiubai_url = "http://www.qiushibaike.com/8hr/page/" + str(page)
+		url = "http://www.qiushibaike.com/8hr/page/" + str(page)
 		
 		#set header
 		user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-		qiubai_header = {'User-Agent': user_agent}
-		qiubai_request = urllib2.Request(qiubai_url, headers=qiubai_header)
-		qiubai_response = urllib2.urlopen(qiubai_request)
-		qiubai_page_content = qiubai_response.read()
+		header = {'User-Agent': user_agent}
+		request = urllib2.Request(url, headers=header)
+		response = urllib2.urlopen(request)
+		page_content = response.read()
 
 		#set code to unicode
-		qiubai_page_content_unicode = qiubai_page_content.decode('utf-8')
+		page_content_unicode = page_content.decode('utf-8')
 
-		qiubai_items = re.findall('<div.*?class="content".*?title="(.*?)">(.*?)</div>',
-			qiubai_page_content_unicode, re.S)
-		qiubai_content_list = []
-		for item in qiubai_items:
-			qiubai_content_list.append([item[0].replace('\n', ''), item[1].replace('\n', '')])
-		return qiubai_content_list
+		items = re.findall('<div.*?class="content".*?title="(.*?)">(.*?)</div>',
+			page_content_unicode, re.S)
+		content_list = []
+		for item in items:
+			content_list.append([item[0].replace('\n', ''), item[1].replace('\n', '')])
+		return content_list
+
+	def load_page(self):
+		while self.enable:
+			if len(self.pages) < 2:
+				print len(self.pages)
+				try:
+					content_list = self.get_page_content(str(self.page))
+					self.page += 1
+					self.pages.append(content_list)
+				except:
+					print 'Unable linked to qiushibaike.com.'
+			else:
+				time.sleep(1)
+
+	def show_page(self, now_page, page):
+		for items in now_page:
+			print 'No.%d' % page, items[0], items[1]
+			quit_input = raw_input()
+			if quit_input == 'q':
+				self.enable = False
+				break
+
+	def start(self):
+		self.enable = True
+		page = self.page
+
+		print 'loding...'
+		thread.start_new_thread(self.load_page, ())
+
+		while self.enable:
+			if self.pages:
+				now_page = self.pages[0]
+				del self.pages[0]
+				self.show_page(now_page, page)
+				page += 1
 
 
 qb = QiubaiCrawler()
-content = qb.get_page_content(1)
-for cont in content:
-	print cont[0], cont[1]
+qb.start()
