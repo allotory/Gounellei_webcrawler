b0ee5e5f0b14e7349a37232dc5c6d2a6a8e875ce
Ellery <allotory@msn.com>
2014-08-25 23:16:57 +0800

    finish basic function

--- zhihu/zhihu_webcrawler.py
+++ zhihu/zhihu_webcrawler.py
@@ -5,3 +5,31 @@ __author__ = 'Ellery Zhang'
 '''
 Zhihu web crawler advanced version.
 '''
+
+import urllib2
+import re
+
+class ZhihuCrawler(object):
+
+	def get_answer(self, page_id):
+		url = "http://www.zhihu.com/question/" + str(page_id)
+		
+		#set header
+		user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+		header = {'User-Agent': user_agent}
+		request = urllib2.Request(url, headers=header)
+		response = urllib2.urlopen(request)
+		page_content = response.read()
+
+		#print page_content
+
+		#set code to unicode
+		page_content_unicode = page_content.decode('utf-8')
+		items = re.findall('<div class=".*?zm-editable-content clearfix">(.*?)</div>',
+			page_content_unicode, re.S)
+		return items
+
+zu = ZhihuCrawler()
+content = zu.get_answer(24984198)
+for c in content:
+	print c
