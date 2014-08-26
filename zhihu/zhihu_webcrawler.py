c3acf92f0181567749e85ff0d9aa16a6a140e412
Ellery <allotory@msn.com>
2014-08-26 23:23:41 +0800

    add question title and question content.

--- zhihu/zhihu_webcrawler.py
+++ zhihu/zhihu_webcrawler.py
@@ -11,6 +11,9 @@ import re
 
 class ZhihuCrawler(object):
 
+	def __init__(self):
+		self.question = []
+
 	def get_answer(self, page_id):
 		url = "http://www.zhihu.com/question/" + str(page_id)
 		
@@ -24,13 +27,18 @@ class ZhihuCrawler(object):
 		#set code to unicode
 		page_content_unicode = page_content.decode('utf-8')
 
-		items = re.findall('<a data-tip=".*?" href="/people/.*?">(.*?)</a>.*?class="zu-question-my-bio">(.*?)</strong>.*?<div class=".*?zm-editable-content clearfix">(.*?)</div>.*?<a class="answer-date-link.*?>(.*?)</a>',
+		title = re.findall('zm-item-title zm-editable-content">(.*?)</h2>.*?<div class="zm-editable-content">(.*?)</div>', page_content_unicode, re.S)
+		#print title[0] + '---' + title[1]
+		self.question = title
+		answers = re.findall('<a data-tip=".*?" href="/people/.*?">(.*?)</a>.*?class="zu-question-my-bio">(.*?)</strong>.*?<div class=".*?zm-editable-content clearfix">(.*?)</div>.*?<a class="answer-date-link.*?>(.*?)</a>',
 			page_content_unicode, re.S)
-		return items
+		return answers
 
 zu = ZhihuCrawler()
 content = zu.get_answer(24997123)
+print zu.question[0][0] + zu.question[0][1]
+print ''
 for c in content:
 	print c[0] + '----' + c[1] + ':'
 	print c[2].lstrip().rstrip() + '----' + c[3]
-	print 
+	print ''
