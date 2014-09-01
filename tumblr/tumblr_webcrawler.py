761a36d4bf3fa5f568d42b38ea38911b5a1cab29
Ellery <allotory@msn.com>
2014-09-01 14:21:06 +0800

    done

--- tumblr/tumblr_webcrawler.py
+++ tumblr/tumblr_webcrawler.py
@@ -11,23 +11,28 @@ import re
 
 class TumblrCrawler(object):
 
-	def __init__(self, url):
+	def __init__(self, url, begin, end):
 		self.tumblr_url = url
+		self.begin_page = begin
+		self.end_page = end
 
 	def getImage(self):
-		tumblr_page = urllib.urlopen(self.tumblr_url).read()
-		imageList = []
-		regex = r'<img src="(.*?)".*?>'
-		image_reg = re.compile(regex)
-		imageList = image_reg.findall(tumblr_page)
-		x = 0
-		print len(imageList)
-		#print imageList
-		for imageurl in imageList:
-			if x < 10:
-				print imageurl
-				urllib.urlretrieve(imageurl,'%s.jpg' % x)
+		for i in range(self.begin_page, self.end_page+1):
+			tumblr_page = urllib.urlopen(self.tumblr_url + 'page/' + str(i)).read()
+			print self.tumblr_url + 'page/' + str(i)
+			imageList = []
+			#original image, this is thumb in cutelygirls.tumblr.com 
+			regex = r'<img src="(.*?)".*?>'
+			#original image in cutelygirls.tumblr.com
+			#regex_ooriginal_inCG = r'href="(.*?)" title="Zoom">'
+			image_reg = re.compile(regex)
+			imageList = image_reg.findall(tumblr_page)
+			x = 0
+			print len(imageList)
+			#print imageList
+			for imageurl in imageList:
+				urllib.urlretrieve(imageurl,'%s_%s.jpg' % (i, x))
 				x += 1
 
-tum = TumblrCrawler('http://cutelygirls.tumblr.com/')
+tum = TumblrCrawler('http://cutelygirls.tumblr.com/', 1, 2)
 tum.getImage()
