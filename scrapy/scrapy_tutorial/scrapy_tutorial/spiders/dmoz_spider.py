667a872e49691e775a1c7573eb9833fc9aef338a
Ellery <allotory@msn.com>
2014-09-03 22:31:38 +0800

    scrapy test done

---
+++ scrapy/scrapy_tutorial/scrapy_tutorial/spiders/dmoz_spider.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+
+from scrapy.spider import Spider
+from scrapy.selector import Selector
+
+class DmozSpider(Spider):
+	name = 'dmoz'
+	allowed_domains = ['dmoz.org']
+	start_urls = [
+		'http://www.dmoz.org/Computers/Programming/Languages/Python/Books/'
+		'http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/'
+	]
+
+	def parse(self, response):
+		#filename = response.url.split('/')[-2]
+		#open(filename, 'wb').write(response.body)
+		sel = Selector(response)
+		sites = sel.xpath('//ul[@class="directory-url"]/li')
+		items = []
+		for site in sites:
+			item = DmozItem()
+			item['title'] = site.xpath('a/text()').extract()
+			item['link'] = site.xpath('a/@href').extract()
+			item['desc'] = site.xpath('text()').extract()
+			items.append(item)
+			return items
